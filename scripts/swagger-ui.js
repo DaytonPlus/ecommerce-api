c4f8fd288c17f6aacaa9d109a52dc5cc08e9594e
DaytonPro <daytonprogrammer@gmail.com>
2025-04-06 11:56:47 -0400

    swagger doc

--- scripts/swagger-ui.js
+++ scripts/swagger-ui.js
@@ -2,30 +2,23 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import startServer from '../src/server.js';
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API Documentada con Swagger',
-      version: '1.0.0',
-      description: 'Swagger UI con Express',
-    },
-    servers: [{ url: `http://localhost:8000` }],
-  },
-  apis: [
-    'src/routes.js',
-    'src/routes/*.js'
-  ],
-};
 
 (async () => {
-  const specs = swaggerJsdoc(options);
-  const app = await startServer();
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
-  /*
-  app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000/api-docs');
-  });
-  */
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+    },
+  },
+  apis: [
+    'docs/api/*.yml',
+  ],
+};
+const specs = swaggerJsdoc(options);
+const app = await startServer();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
 })();
