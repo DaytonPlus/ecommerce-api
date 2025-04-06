cb590d13dab38204b39388e24e2f11cce630ab75
DaytonPro <daytonprogrammer@gmail.com>
2025-04-05 23:13:28 -0400

    v

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'jest',
-  testEnvironment: 'node',
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  testPathIgnorePatterns: ['/node_modules/'],
-  setupFilesAfterEnv: ['./tests/setup.js'],
-};
